7d70e4278e5676c8e4d9545000fad84b13f59bba
known <mondo.beyondo3@gmail.com>
2023-11-16 02:10:51 -0500

    Implement high level abstractions for printing to screen and getting cartesian coordinates from a string of numbers

--- main.c
+++ main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/ioctl.h>
+#include <stdbool.h>
+#include <time.h>
+#include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
-struct wh {
+// Used to get unbuffered character input from user, solution found from:
+// https://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
+char getch() {
+  char buf = 0;
+
+  struct termios old = {0};
+
+  if (tcgetattr(0, &old))
+    perror("tcsetattr()");
+
+  old.c_lflag &= ~ICANON;
+  old.c_lflag &= ~ECHO;
+  old.c_cc[VMIN] = 1;
+  old.c_cc[VTIME] = 0;
+
+  if (tcsetattr(0, TCSANOW, &old) < 0)
+    perror("tcsetattr ICANON");
+
+  if (read(0, &buf, 1) < 0)
+    perror("read()");
+
+  old.c_lflag |= ICANON;
+  old.c_lflag |= ECHO;
+
+  if (tcsetattr(0, TCSADRAIN, &old) < 0)
+    perror("tcsettattr ~ICANON");
+
+  return buf;
+}
+
+struct dimension {
   int width;
   int height;
 };
@@ -13,51 +46,100 @@ struct point {
   int y;
 };
 
-struct wh get_dimensions() {
+typedef struct Cell {
+  struct point pos;
+  bool is_filled;
+  char color;
+} Cell;
+
+
+
+struct dimension get_dimensions() {
   struct winsize w;
 
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
-  struct wh wh = { w.ws_row, w.ws_col };
+  // At this time row - 2 provides enough separation from the sides of the terminal window as to be visually pleasing
+  struct dimension wh = { w.ws_col, w.ws_row - 2 };
 
   return wh;
 }
 
-struct point to_point(int input, struct wh bounds) {
-  printf("TODO");
 
-  struct point p;
+
+struct point to_point(int input, struct dimension bounds) {
+  struct point p = { input % bounds.width, input / bounds.width };
 
   return p;
 }
 
-// char* create_board(struct wh wh) {
-//   const int SIZE = wh.height * wh.width;
-//   char* board = malloc(SIZE);
 
-//   for (int i = 0; i < SIZE; i++)
-//     board[i] = '*';
 
-  
+int from_point(struct point p, struct dimension bounds) {
+  return (p.y * bounds.width) + p.x;
+}
 
-//   for (int i = 0; i < wh.width; i++) {
-//     for (int j = 0; j < wh.height; j++) {
-//       if (wh.width / 2 == i && wh.height / 2 == j)
-//         printf("%c", board[i][j]);
-//       else
-//         printf(" ");
-//     }
 
-//     printf("\n");
-//   }
 
-//   return board;
-// }
+Cell* create_board(struct dimension bounds) {
+  const int SIZE = bounds.height * bounds.width;
+
+  Cell* board = malloc(sizeof(Cell) * SIZE);
+
+  for (int i = 0; i < SIZE; i++) {
+
+    // These are the bounds of the screen
+    if (i % bounds.width == 0 || i / bounds.width == 0 || i % bounds.width == (bounds.width - 1) ||  i / bounds.width == (bounds.height - 1)) {
+
+      struct point p = to_point(i, bounds);
+      Cell c = { p, true, '#' };
+      board[i] = c;
+
+    } else {
+
+      struct point p = to_point(i, bounds);
+      Cell c = { p, false, ' ' };
+      board[i] = c;
+
+    }
+  }
+
+  return board;
+}
+
+
 
 int main(int argc, char** argv) {
-  struct wh wh = get_dimensions();
+  srand(time(NULL));
+  struct dimension bounds = get_dimensions();
 
+  Cell* board = create_board(bounds);
 
-  
+  Cell player = { { (rand() % (bounds.width - 1)) + 1, (rand() % (bounds.height - 1)) + 1 }, true, '&' };
+
+  board[from_point(player.pos, bounds)] = player;
+
+  while (1) {
+    printf("\033[2J");
+
+    for (int i = 0; i < bounds.height * bounds.width; i++) {
+      printf("%c", board[i].color);
+      if ((i + 1) % bounds.width == 0) {
+        printf("\n");
+      }
+    }
+
+    Cell clear = { player.pos, false, ' ' };
+
+    board[from_point(player.pos, bounds)] = clear;
+
+    player.pos.x = (rand() % (bounds.width - 1)) + 1;
+    player.pos.y = (rand() % (bounds.height - 1)) + 1;
+
+    board[from_point(player.pos, bounds)] = player;
+
+    sleep(1);
+  }
+
   return 0;
 }
